0662e461b33b2466eb919a2519a1929c58eebf51
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-20 17:51:59 +0530

    sv: Initialize stripe client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hi')
+// Environment Variables
+import { config } from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+  config()
+}
+
+// Initialize stripe
+import Stripe from 'stripe'
+export const stripe = new Stripe(process.env.STRIPE_SECRET, {
+  apiVersion: '2020-08-27',
+})
