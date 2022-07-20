85406ce4c62b84852453f37e3af60766c8f1cafe
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-20 18:12:18 +0530

    sv: Add a test route sample

--- server/src/api.ts
+++ server/src/api.ts
@@ -6,4 +6,8 @@ export const app = express()
 // Middleware
 app.use(cors({ origin: true }))
 app.use(express.json())
+
+app.post('/test', (request: Request, response: Response) => {
+  const { amount } = request.body
+  response.status(200).send({ withTax: amount * 5 })
 })
