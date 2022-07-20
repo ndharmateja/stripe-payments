a4d3d35ee83fb51aea16078abc972c18abe273e5
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-20 21:52:22 +0530

    sv: Add /checkout post route

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from 'express'
 import cors from 'cors'
+import { createStripeCheckoutSession } from './checkout'
 
 export const app = express()
 
@@ -11,3 +12,20 @@ app.post('/test', (request: Request, response: Response) => {
   const { amount } = request.body
   response.status(200).send({ withTax: amount * 5 })
 })
+
+app.post(
+  '/checkout',
+  asyncWrapper(async (request: Request, response: Response) => {
+    const {
+      body: { line_items: lineItems },
+    } = request
+    const session = await createStripeCheckoutSession(lineItems)
+    response.send(session)
+  })
+)
+
+function asyncWrapper(callback: Function) {
+  return (request: Request, response: Response, next: NextFunction) => {
+    callback(request, response, next).catch(next)
+  }
+}
