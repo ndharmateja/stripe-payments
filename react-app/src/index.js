97afdb322b881572ed55d361296a10f4a6af83a9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-20 19:25:58 +0530

    ra: Add code to load stripe

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
+export const stripePromise = loadStripe(
+  'pk_test_51LNax4SHcwLCs0BdYXNhkcnJGnyunN3CpJfOdZOYNAfRN1zZelEgtxC7s0T2luyvL4xfuNGEaQ0s69biw3mcwL3800SM7mjjho'
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </React.StrictMode>
 )
