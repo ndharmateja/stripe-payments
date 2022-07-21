9cccd6edb00145b5633c40d01034a807a8b3c7d9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-21 15:06:12 +0530

    ra: Retrieve search param from url

--- react-app/src/pages/CheckoutSuccess.js
+++ react-app/src/pages/CheckoutSuccess.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const CheckoutSuccess = () => {
-  return <div>CheckoutSuccess</div>
+  const url = window.location.href
+  const sessionId = new URL(url).searchParams.get('session_id')
+  return <h3>Checkout success! {sessionId}</h3>
 }
 
 export default CheckoutSuccess
