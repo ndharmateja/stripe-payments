548a27caf09530474babb9eee06d27fe37e77f07
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-21 14:16:15 +0530

    ra: Add the product to the checkout page

--- react-app/src/pages/Checkout.js
+++ react-app/src/pages/Checkout.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Checkout = () => {
-  return <div>Checkout</div>
+
+  const [product, setProduct] = useState({
+    name: 'Hat',
+    description: 'Pug hat. A hat your pug will love.',
+    images: [
+      'https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    ],
+    amount: 799,
+    currency: 'usd',
+    quantity: 0,
+  })
+
+  const { name, amount, images, quantity } = product
+
+  const changeQuantity = (increment) =>
+    setProduct({
+      ...product,
+      quantity: Math.max(0, product.quantity + increment),
+    })
+
+  const handleClick = async () => {
+    window.alert('you clicked')
+  }
+
+  return (
+    <>
+      <div>
+        <h3>{name}</h3>
+        <h4>Stripe amount: {amount}</h4>
+        <img src={images[0]} alt='product' width='250px' />
+
+        <button onClick={() => changeQuantity(-1)}>-</button>
+        <span>{quantity}</span>
+        <button onClick={() => changeQuantity(1)}>+</button>
+      </div>
+
+      <hr />
+
+      <button onClick={handleClick} disabled={quantity < 1}>
+        Start Checkout
+      </button>
+    </>
+  )
 }
 
 export default Checkout
