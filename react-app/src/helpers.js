510731aa1f0d1da7b8432a9ae96a9f50809bc2e0
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-31 05:41:24 +0530

    ra: Add auth headers to requests

--- react-app/src/helpers.js
+++ react-app/src/helpers.js
@@ -1,13 +1,18 @@
+import { auth } from './firebase'
 const API = 'http://localhost:3003'
 
 export const fetchFromApi = async (endpoint, options) => {
   const { method, body } = { method: 'POST', body: null, ...options }
+
+  const user = auth.currentUser
+  const token = user && (await user.getIdToken())
 
   const response = await fetch(`${API}/${endpoint}`, {
     method,
     ...(body && { body: JSON.stringify(body) }),
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
     },
   })
 
