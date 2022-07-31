eafd922bf49138aac393ce868da6a20412fa2827
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-31 05:41:12 +0530

    ra: Fix backward compatibility issue

--- react-app/src/firebase.js
+++ react-app/src/firebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD9RZYV6b5IDqJfHHVSOVjywqcB8pNeb3A',
@@ -14,5 +14,5 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 
-export const db = firebase.firestore()
 export const auth = firebase.auth()
+export const db = firebase.firestore()
